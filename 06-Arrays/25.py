04680bf2e06a02354642b84c0e9ffad2466bca6d
Bartosz Lipiński <93048173+bartoszlipinski@users.noreply.github.com>
2021-11-19 13:37:04 +0100

    Update 25.py

--- 06-Arrays/25.py
+++ 06-Arrays/25.py
@@ -2,6 +2,7 @@ array = [4,2,8,4,7,9,5]
 
 def minmax(arr):
     arr.sort()
-    return [arr[0], arr[len(arr)-1]]
+    newArray = [arr[0], arr[len(arr)-1]]
+    return newArray
 
 print(minmax(array))
